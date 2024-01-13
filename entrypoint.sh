2b83f73aa174af803fbf403004e4d64dd917217a
Kasai Kou <80010105+kasaikou@users.noreply.github.com>
2024-01-13 04:54:17 +0000

    startup: delete max-page

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,6 @@ DEST=$2
 MAX_PAGES=$3
 
 export DEFAULT_LANG=$4
+export OCR_ENGINE=ocrmypdf
 
-.venv/bin/python convert_single.py ${GITHUB_WORKSPACE}/${SRC} ${GITHUB_WORKSPACE}/${DEST} --max_pages ${MAX_PAGES}
+.venv/bin/python convert_single.py ${GITHUB_WORKSPACE}/${SRC} ${GITHUB_WORKSPACE}/${DEST}
